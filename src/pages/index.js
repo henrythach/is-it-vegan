2861e177a13e35497a2d248a0834a047d51668c4
Henry Thach <thachhao@gmail.com>
2019-03-03 17:27:21 -0500

    Initial list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
-import { Typography, TextField } from '@material-ui/core'
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar
+} from '@material-ui/core'
+import CheckIcon from '@material-ui/icons/Check'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
+    <List>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <CheckIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary='Single-line item'
+          secondary='Not vegan'
+        />
+      </ListItem>
+    </List>
   </Layout>
 )
 
