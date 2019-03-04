ad27afe7658734c2137f56cf077507166e1b2a38
Henry Thach <thachhao@gmail.com>
2019-03-04 06:26:59 -0500

    Make index.js to main app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { List, ListItem, ListItemText } from '@material-ui/core'
-import * as data from '../data/non-vegan.json'
+import {
+  List,
+  ListItem,
+  ListItemText,
+  AppBar,
+  Toolbar,
+  InputBase,
+  CssBaseline
+} from '@material-ui/core'
+import * as ingredients from '../data/non-vegan.json'
+import { withStyles } from '@material-ui/core/styles'
+import SearchIcon from '@material-ui/icons/Search'
+
+const styles = theme => ({
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200
+      }
+    }
+  }
+})
 
 const Ingredient = ({ title }) => (
   <ListItem>
@@ -11,15 +39,43 @@ const Ingredient = ({ title }) => (
   </ListItem>
 )
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
-    <List dense>
-      {data.map((d, index) => (
-        <Ingredient key={index} title={d} />
-      ))}
-    </List>
-  </Layout>
+const Ingredients = ({ ingredients }) => (
+  <List dense>
+    {ingredients.map((d, index) => (
+      <Ingredient key={index} title={d} />
+    ))}
+  </List>
 )
 
-export default IndexPage
+const IndexPage = ({ classes }) => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const filteredIngredients = ingredients
+    .filter(i => i.toLowerCase().includes(searchQuery.trim().toLowerCase()))
+    .slice(0, 25)
+
+  return (
+    <>
+      <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
+      <CssBaseline />
+      <AppBar position='static'>
+        <Toolbar>
+          <SearchIcon />
+          <InputBase
+            placeholder='Search ingredient'
+            fullWidth
+            autoFocus
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput
+            }}
+            value={searchQuery}
+            onChange={event => setSearchQuery(event.target.value)}
+          />
+        </Toolbar>
+      </AppBar>
+      <Ingredients ingredients={filteredIngredients} />
+    </>
+  )
+}
+
+export default withStyles(styles)(IndexPage)
