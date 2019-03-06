24e27211a104a6facd2ddf94f9a339b1132e8fcd
Henry Thach <thachhao@gmail.com>
2019-03-06 04:58:28 -0500

    Update manifest title and display

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `is-it-vegan`,
+        short_name: `Is It Vegan?`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
